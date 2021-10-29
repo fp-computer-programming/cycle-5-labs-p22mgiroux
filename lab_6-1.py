ab95744b67c05ada1b82120ea3c5a813c0e2538d
p22mgiroux <p22mgiroux@fairfieldprep.org>
2021-10-29 10:45:37 -0400

    finish lab 6

---
+++ lab_6-1.py
@@ -0,0 +1,7 @@
+# Author: MOG 10/29/21
+
+word = "flibbertigibbet"
+print(word.find("t"))
+
+sentence = "I wish, I wish, I was a fish."
+print(sentence.split(", "))
---
+++ lab_6-2.py
@@ -0,0 +1,5 @@
+# Author: MOG 10/29/21
+
+person1 = input("Person 1 first name: ")
+person2 = input("Person 2 first name: ")
+print("Hello, {}. My name is {}.".format(person1, person2))
