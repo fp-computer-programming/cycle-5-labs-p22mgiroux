d479d6c8a0b4e5d90a87b3dbd8bb94f587c6163c
p22mgiroux <p22mgiroux@fairfieldprep.org>
2021-10-29 10:02:56 -0400

    finish lab 4-1

---
+++ lab_4-1.py
@@ -0,0 +1,15 @@
+# Author: MOG 10/29/21
+
+fairfield = "Fairfield"
+prep ="Prep"
+fairfield_prep = fairfield + " " + prep
+print(fairfield_prep)
+
+dash = "-"
+long_dash = dash * 20
+print(long_dash)
+
+print('''I'm loving this short story I'm reading, "The Fall of the House of Usher"''')
+
+apple = "apple"
+print(apple[3])
